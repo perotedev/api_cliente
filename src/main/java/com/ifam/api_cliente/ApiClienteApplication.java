f6d15595d8a9b92fe0fa6fe90f5e59efaa8becbb
rodrigoperote <rodripema@hotmail.com>
2022-04-29 14:34:04 -0400

    [INIT] - Commit Inicial

---
+++ src/main/java/com/ifam/api_cliente/ApiClienteApplication.java
@@ -0,0 +1,12 @@
+package com.ifam.api_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClienteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ifam/cliente/api_cliente/ApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifam.cliente.api_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
